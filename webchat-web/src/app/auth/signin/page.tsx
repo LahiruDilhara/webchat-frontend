0c784dd430ab3cca72f4fdc339bd6a209cb51cee
Lahirukasunidilhara <lahirukasunidilhara@Lahirukasunidilhara.lahirudilhara.me>
2025-09-24 23:41:50 +0530

    created the lg view of sign in and sign up

--- webchat-web/src/app/auth/signin/page.tsx
+++ webchat-web/src/app/auth/signin/page.tsx
@@ -14,18 +14,22 @@ export default function SignInPage() {
     const router = useRouter();
 
     return (
-        <div className="flex flex-col px-md gap-md h-full min-h-0 items-center justify-center md:max-w-4/6">
-            <div className="w-full flex-1 flex flex-col items-center justify-center gap-sm">
-                <div className="text-h1">Welcome Back!</div>
-                <div className="text-caption">Please sign in to your account.</div>
+        <div className="flex flex-col px-md gap-md h-full min-h-0 items-center justify-center md:max-w-4/6 lg:w-full lg:max-w-full lg:grid lg:grid-cols-20 lg:p-lg">
+            <div className="w-full flex-1 flex flex-col items-center justify-center gap-sm lg:col-span-12">
+                <div className="text-h1">Welcome Back !</div>
+                <div className="text-caption lg:hidden">Please sign in to your account.</div>
             </div>
-            <form className="flex-2 w-full flex flex-col gap-4xl" onSubmit={e => { e.preventDefault(); }}>
+            <form className="flex-2 w-full flex flex-col gap-4xl lg:gap-2xl lg:col-span-8" onSubmit={e => { e.preventDefault(); }}>
+                <div className="w-full flex-col hidden lg:block justify-center items-center text-center">
+                    <h1 className="text-h3">Login To Your Account</h1>
+                    <div className="text-caption">Please sign in to your account.</div>
+                </div>
                 <div className="flex flex-col gap-xl">
                     <LabelInput label="Username" placeholder="username" value={username} onChange={(e) => { updateUsername(e.target.value) }} inputType="text"></LabelInput>
                     <LabelInput label="Password" placeholder="password" value={password} onChange={(e) => { updatePassword(e.target.value) }} inputType="password"></LabelInput>
                 </div>
                 <div className="flex flex-row w-full justify-between gap-xl">
-                    <IconOutlinedButton icon={<UserRoundPlus />} label="Sign Up" onClick={() => { router.replace("/auth/signup")}} className="flex-1 text-nowrap"></IconOutlinedButton>
+                    <IconOutlinedButton icon={<UserRoundPlus />} label="Sign Up" onClick={() => { router.replace("/auth/signup") }} className="flex-1 text-nowrap"></IconOutlinedButton>
                     <IconButton icon={isLoading ? <ClipLoader size={24} /> : <LogIn />} label="Sign In" onClick={submit} className="flex-1 text-nowrap"></IconButton>
                 </div>
             </form>
--- webchat-web/src/app/auth/signup/page.tsx
+++ webchat-web/src/app/auth/signup/page.tsx
@@ -12,12 +12,16 @@ export default function SignUpPage() {
     const router = useRouter();
 
     return (
-        <div className="flex flex-col px-md gap-md h-full min-h-0 items-center justify-center md:max-w-4/6">
-            <div className="w-full flex-1 flex flex-col items-center justify-center gap-sm">
-                <div className="text-h1">Let's Get Started</div>
-                <div className="text-caption">Please sign up for a new account.</div>
+        <div className="flex flex-col px-md gap-md h-full min-h-0 items-center justify-center md:max-w-4/6 lg:w-full lg:max-w-full lg:grid lg:grid-cols-20 lg:p-lg">
+            <div className="w-full flex-1 flex flex-col items-center justify-center gap-sm lg:col-span-12">
+                <div className="text-h1">Let's Get Started ...</div>
+                <div className="text-caption lg:hidden">Please sign up for a new account.</div>
             </div>
-            <form className="flex-2 w-full flex flex-col gap-4xl" onSubmit={e => { e.preventDefault(); }}>
+            <form className="flex-2 w-full flex flex-col gap-4xl lg:gap-2xl lg:col-span-8" onSubmit={e => { e.preventDefault(); }}>
+                <div className="w-full flex-col hidden lg:block justify-center items-center text-center">
+                    <h1 className="text-h3">Create Your Account</h1>
+                    <div className="text-caption">Please sign up for a new account.</div>
+                </div>
                 <div className="flex flex-col gap-xl">
                     <LabelInput label="Username" placeholder="username" value={username} onChange={(e) => { updateUsername(e.target.value) }} inputType="text"></LabelInput>
                     <LabelInput label="Password" placeholder="password" value={password} onChange={(e) => { updatePassword(e.target.value) }} inputType="password"></LabelInput>
