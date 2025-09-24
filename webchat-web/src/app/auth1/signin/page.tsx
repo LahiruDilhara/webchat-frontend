3dd248e05ae8a32e698c12462db4e245e2fc99ad
Lahirukasunidilhara <lahirukasunidilhara@Lahirukasunidilhara.lahirudilhara.me>
2025-09-24 20:12:40 +0530

    add the button styles and standard the styles

--- webchat-web/src/app/auth1/signin/page.tsx
+++ webchat-web/src/app/auth1/signin/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import IconLabelButton from "@/components/primitive/IconLabelButton";
 import LabelInput from "@/components/primitive/LabelInput";
+import { LogIn } from "lucide-react";
 
 
 export default function SignInPage() {
@@ -9,7 +11,7 @@ export default function SignInPage() {
         <form onSubmit={e => { e.preventDefault(); }}>
             <LabelInput label="Username" placeholder="username" value="" onChange={() => { }}></LabelInput>
             <LabelInput label="Username" placeholder="username" value="" onChange={() => { }}></LabelInput>
-            <h1 className="text-h1">Hello world</h1>
+            <IconLabelButton icon={<LogIn></LogIn>} label="Sign In" onClick={() => { }}></IconLabelButton>
         </form>
     </>;
 }
--- webchat-web/src/components/primitive/IconLabelButton.tsx
+++ webchat-web/src/components/primitive/IconLabelButton.tsx
@@ -4,11 +4,17 @@ type Props = {
     icon: React.ReactNode;
     label: string;
     onClick: () => void;
+    iconPosition?: 'left' | 'right';
+    className?: string;
 }
-const IconLabelButton = () => {
-    return (<>
-        <Caravan></Caravan>
-    </>);
+const IconLabelButton = ({ icon, label, onClick, iconPosition = 'left', className }: Props) => {
+    return (
+        <button onClick={onClick} className={`flex min-h-0 min-w-0 items-center gap-sm bg-primary px-sm py-xs rounded-md hover:brightness-75 cursor-pointer ${className}`}>
+            {iconPosition === 'left' && icon}
+            <h1 className="text-button">{label}</h1>
+            {iconPosition === 'right' && icon}
+        </button>
+    );
 }
 
 export default IconLabelButton;
