f40a80bb0501371eb7423fd7ffe59f89da767bb8
Lahirukasunidilhara <lahirukasunidilhara@Lahirukasunidilhara.lahirudilhara.me>
2025-09-25 21:32:24 +0530

    created the chat desktop version

--- webchat-web/src/app/home/mdHomePage.tsx
+++ webchat-web/src/app/home/mdHomePage.tsx
@@ -6,8 +6,8 @@ const MdHomePage = () => {
     const [folded, setFolded] = useState(false);
     return (
         <div className="w-full h-full grid grid-rows-[1fr_15fr] py-md">
-            <div className="flex flex-row items-center min-h-0 min-w-0 w-full">
-                <div className="flex flex-row items-center gap-md w-full">
+            <div className="flex flex-row min-h-fit items-center min-w-0 w-full">
+                <div className="flex flex-row min-h-fit items-center gap-md w-full">
                     <div className="cursor-pointer" onClick={() => setFolded(!folded)}>
                         <Menu />
                     </div>
@@ -62,7 +62,30 @@ const MdHomePage = () => {
                     </div>
                 </div>
             </div>
-            <div className=""></div>
+            <div className={`grid gap-lg pt-lg ${folded ? "grid-cols-1" : "grid-cols-[2fr_7fr]"} h-full overflow-hidden`}>
+                {!folded && (
+                    <div className="flex flex-col min-h-0 min-w-0 gap-md">
+                        <h1 className="text-body">Rooms</h1>
+                        <div className="w-full flex items-center justify-center">
+                            <SearchInput placeholder="Search..." value="" onChange={(val) => console.log(val)} className=" w-full" />
+                        </div>
+                        <div className="flex flex-col gap-lg overflow-y-scroll ">
+                            <div className="w-full shrink-0 flex flex-row gap-md min-h-0 bg-card-bg p-sm rounded-lg cursor-pointer">
+                                <div className="shrink-0 size-2xl bg-amber-200 rounded-full"></div>
+                                <div className="w-full flex flex-col justify-between">
+                                    <h1 className="text-body">room 1</h1>
+                                    <div className="flex flex-row justify-between w-full">
+                                        <h1 className="text-caption hidden lg:block">private</h1>
+                                        <h1 className="text-caption">2025/12/08</h1>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                )}
+                <div className="w-full h-full rounded-lg bg-card-bg"></div>
+            </div>
         </div>
     );
 }
--- webchat-web/src/app/home/smHomePage.tsx
+++ webchat-web/src/app/home/smHomePage.tsx
@@ -3,14 +3,14 @@ import { Plus } from "lucide-react";
 
 const SmHomePage = () => {
     return (
-        <div className="w-full h-full grid grid-rows-[1fr_1fr_7fr] gap-md py-md">
+        <div className="w-full h-full grid grid-rows-[1fr_auto_7fr] gap-md py-md">
             <div className="flex flex-col gap-sm">
                 <h1 className="text-h2">Chat</h1>
                 <div className="w-full flex items-center justify-center">
                     <SearchInput placeholder="Search..." value="" onChange={(val) => console.log(val)} className=" w-full" />
                 </div>
             </div>
-            <div className="w-full flex flex-row gap-md overflow-x-auto">
+            <div className="w-full flex min-h-fit flex-row gap-md overflow-x-auto">
                 <div className="flex flex-col justify-center items-center text-center  cursor-pointer">
                     <div className="shrink-0 size-2xl flex justify-center items-center border-2 border-primary hover:bg-primary rounded-full">
                         <Plus size={16}></Plus>
