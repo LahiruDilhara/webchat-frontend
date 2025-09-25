fcad36ed9dcc07aef88329e390c7b440a1c306af
Lahirukasunidilhara <lahirukasunidilhara@Lahirukasunidilhara.lahirudilhara.me>
2025-09-25 15:42:57 +0530

    make the add room button clickable

--- webchat-web/src/app/home/page.tsx
+++ webchat-web/src/app/home/page.tsx
@@ -5,7 +5,7 @@ import { EllipsisVertical, Hamburger, Menu, Plus } from "lucide-react";
 
 const HomePage = () => {
     return (
-        <div className="w-full h-full grid grid-rows-[1fr_1fr_7fr] gap-sm py-md">
+        <div className="w-full h-full grid grid-rows-[1fr_1fr_7fr] gap-md py-md">
             <div className="flex flex-col gap-sm">
                 <h1 className="text-h2">Chat</h1>
                 <div className="w-full flex items-center justify-center">
@@ -13,8 +13,8 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="w-full flex flex-row gap-md overflow-x-auto">
-                <div className="flex flex-col justify-center items-center text-center">
-                    <div className="shrink-0 size-2xl flex justify-center items-center border-2 border-primary rounded-full">
+                <div className="flex flex-col justify-center items-center text-center  cursor-pointer">
+                    <div className="shrink-0 size-2xl flex justify-center items-center border-2 border-primary hover:bg-primary rounded-full">
                         <Plus size={16}></Plus>
                     </div>
                     <h1 className="text-caption text-nowrap">add room</h1>
@@ -25,7 +25,7 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="flex flex-col min-h-0">
-                <h1 className="text-h3 pt-md pb-sm">Rooms</h1>
+                <h1 className="text-h3 pb-sm">Rooms</h1>
                 <div className="flex flex-col gap-lg overflow-y-scroll min-h-0">
                     <div className="w-full shrink-0 flex flex-row gap-md min-h-0">
                         <div className="shrink-0 size-2xl bg-amber-200 rounded-full"></div>
