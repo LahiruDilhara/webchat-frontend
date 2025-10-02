2c9e12722690187fcce5351876ddc1cc1bc8921c
lahirudilhara <galahirudilhara@gmail.com>
2025-10-02 17:47:16 +0530

    added the text on the medium home page column circles

--- webchat-web/src/app/home/components/md/roomColumnItem.tsx
+++ webchat-web/src/app/home/components/md/roomColumnItem.tsx
@@ -14,7 +14,7 @@ type Props = {
 const MdRoomColumnItem = ({ caption, date, name, onRoomClick, id, activeRoomId, count }: Props) => {
     return (
         <div className={`w-full shrink-0 flex flex-row gap-md min-h-0 bg-card-bg p-sm rounded-lg cursor-pointer hover:brightness-75 ${activeRoomId === id ? "brightness-75" : ""}`} onClick={() => onRoomClick(id)}>
-            <div className={`shrink-0 size-2xl ${getColorForString(name)} rounded-full`}></div>
+            <div className={`shrink-0 size-2xl ${getColorForString(name)} rounded-full flex items-center justify-center text-body`}>{name.charAt(0).toUpperCase()}</div>
             <div className="w-full flex flex-col justify-between">
                 <div className="flex flex-row justify-between w-full">
                     <h1 className="text-body">{name}</h1>
