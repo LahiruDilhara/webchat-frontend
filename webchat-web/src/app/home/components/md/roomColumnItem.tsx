4b7ab464d3e01fce987bfced40a97b7482f76e44
lahirudilhara <galahirudilhara@gmail.com>
2025-10-03 01:33:38 +0530

    make the medium room icon text size is larger

--- webchat-web/src/app/home/components/md/roomColumnItem.tsx
+++ webchat-web/src/app/home/components/md/roomColumnItem.tsx
@@ -15,7 +15,7 @@ type Props = {
 const MdRoomColumnItem = ({ caption, date, name, onRoomClick, id, activeRoomId, count }: Props) => {
     return (
         <div className={`w-full shrink-0 flex flex-row gap-md min-h-0 bg-card-bg p-sm rounded-lg cursor-pointer hover:brightness-75 ${activeRoomId === id ? "brightness-75" : ""}`} onClick={() => onRoomClick(id)}>
-            <div className={`shrink-0 size-2xl ${getColorForString(name)} rounded-full flex items-center justify-center text-body`}>{name.charAt(0).toUpperCase()}</div>
+            <div className={`shrink-0 size-2xl ${getColorForString(name)} rounded-full flex items-center justify-center text-h3`}>{name.charAt(0).toUpperCase()}</div>
             <div className="w-full flex flex-col justify-between">
                 <div className="flex flex-row justify-between w-full">
                     <h1 className="text-body">{capitalizeFirstLetter(name)}</h1>
