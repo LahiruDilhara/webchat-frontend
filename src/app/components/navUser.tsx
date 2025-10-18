d66e6d9ca56a70283e9f99d4ae346669e2db03d7
lahirudilhara <galahirudilhara@gmail.com>
2025-10-18 20:36:46 +0530

    change the nav bar color and make it look good

--- src/app/components/navUser.tsx
+++ src/app/components/navUser.tsx
@@ -26,9 +26,9 @@ const NavUser = () => {
     }, [])
 
     return (
-        <div className="flex flex-row items-center gap-sm cursor-pointer relative h-full brightness-75 bg-primary px-md z-[1001]" onClick={() => setProfile(!profile)} ref={menuRef}>
-            <h1 className={`${getColorForString(username)} rounded-full size-lg text-caption text-center flex items-center justify-center`}>{username.charAt(0)}</h1>
-            <div className="flex flex-row items-center gap-xs">
+        <div className="flex flex-row items-center gap-sm cursor-pointer relative h-full brightness-75 px-md z-[1001]" onClick={() => setProfile(!profile)} ref={menuRef}>
+            <h1 className={`${getColorForString(username)} rounded-full size-lg text-caption text-center  flex items-center justify-center`}>{username.charAt(0)}</h1>
+            <div className="flex flex-row items-center gap-xs ">
                 <h1 className="text-caption">Welcome {username}</h1>
                 <ChevronDown size={16} />
             </div>
